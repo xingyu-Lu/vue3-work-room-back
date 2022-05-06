5db9eca57e24934e0900037cfd96f46285483ebd
xingyu-Lu <2273466984@qq.com>
2022-05-06 15:56:00 +0800

    update

--- src/views/technicaloffice/outpatient/add.vue
+++ src/views/technicaloffice/outpatient/add.vue
@@ -1,50 +1,8 @@
 <template>
 	<el-card style="min-height: 100%;">
-		<el-form :model="Form" :rules="rules" ref="Ref" label-width="100px">
-			<el-form-item label="科室" prop="office_id">
-				<el-select v-model="Form.office_id" placeholder="Select" filterable>
-					<el-option v-for="item in Form.list" :key="item.id" :label="item.name" :value="item.id">
-					</el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="院区" prop="yq_type">
-				<el-select v-model="Form.yq_type" placeholder="Select" filterable>
-					<el-option value="0" label="院本部"></el-option>
-					<el-option value="1" label="李庄院区"></el-option>
-				</el-select>
-			</el-form-item>
-			<el-form-item label="周一" prop="monday">
-				<el-input v-model="Form.monday" placeholder="请输入周一门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周二" prop="tuesday">
-				<el-input v-model="Form.tuesday" placeholder="请输入周二门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周三" prop="wednesday">
-				<el-input v-model="Form.wednesday" placeholder="请输入周三门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周四" prop="thursday">
-				<el-input v-model="Form.thursday" placeholder="请输入周四门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周五" prop="friday">
-				<el-input v-model="Form.friday" placeholder="请输入周五门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周六" prop="saturday">
-				<el-input v-model="Form.saturday" placeholder="请输入周六门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="周天" prop="sunday">
-				<el-input v-model="Form.sunday" placeholder="请输入周天门诊医生"></el-input>
-			</el-form-item>
-			<el-form-item label="上下午" prop="type">
-				<el-radio-group v-model="Form.type">
-					<el-radio label=0>上午</el-radio>
-					<el-radio label=1>下午</el-radio>
-				</el-radio-group>
-			</el-form-item>
-			<el-form-item label="上架状态" prop="status">
-				<el-radio-group v-model="Form.status" disabled>
-					<el-radio label=1>开启</el-radio>
-					<el-radio label=0>禁用</el-radio>
-				</el-radio-group>
+		<el-form :model="briefForm" :rules="rules" ref="outpatientRef" label-width="100px" style="margin-top: 30px;">
+			<el-form-item label="内容" prop="content">
+				<div ref='editor'></div>
 			</el-form-item>
 			<el-form-item>
 				<el-button type="primary" @click="submitAdd()">{{ id ? '立即修改' : '立即创建' }}</el-button>
@@ -55,10 +13,14 @@
 
 <script>
 	import {
+		Plus,
+	} from '@element-plus/icons-vue'
+	import {
 		reactive,
 		ref,
 		toRefs,
 		onMounted,
+		onBeforeUnmount,
 	} from 'vue'
 	import WangEditor from 'wangeditor'
 	import axios from '@/utils/axios'
@@ -69,130 +31,152 @@
 		useRoute,
 		useRouter
 	} from 'vue-router'
-	
+	import {
+		sessionGet,
+		uploadImgsServer
+	} from '@/utils'
+
 	export default {
-		name: 'technicaloffice_setting_add',
 		setup() {
-			const Ref = ref(null)
+			const editor = ref(null)
+			const outpatientRef = ref(null)
 			const route = useRoute()
 			const router = useRouter()
 			const {
 				id
 			} = route.query
-			
 			const state = reactive({
+				token: 'Bearer ' + sessionGet('token') || '',
 				id: id,
-				Form: {
-					office_id: '',
-					yq_type: '',
-					monday: '',
-					tuesday: '',
-					wednesday: '',
-					thursday: '',
-					friday: '',
-					saturday: '',
-					sunday: '',
-					type: '0',
-					yq_type: '0',
-					status: '0',
-					list: [],
+				briefForm: {
+					content: '',
 				},
-				
+
 				rules: {
-					office_id: [{
-						required: 'true',
-						message: '科室名称必须',
-						trigger: ['change'],
+					content: [{
+						required: false,
+						message: '内容必须',
+						trigger: ['change']
 					}],
-					type: [{
-						required: 'true',
-						message: '上下午必须',
-						trigger: ['change'],
-					}],
-					yq_type: [{
-						required: 'true',
-						message: '院区',
-						trigger: ['change'],
-					}],
-					status: [{
-						required: 'true',
-						message: '状态必须',
-						trigger: ['change'],
-					}],
-				}
+				},
 			})
-			
-			onMounted(() => {		
-				get_technicaloffice_list()
+
+			let instance
+			onMounted(() => {
+				instance = new WangEditor(editor.value)
+				instance.config.lineHeights = ['1', '1.15', '1.5', '2', '2.5', '3']
+				instance.config.showLinkImg = false
+				instance.config.showLinkImgAlt = false
+				instance.config.showLinkImgHref = false
+				instance.config.uploadImgMaxSize = 1 * 1024 * 1024 // 5M
+				instance.config.uploadImgMaxLength = 1
+				instance.config.uploadImgAccept = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp']
+				instance.config.uploadFileName = 'file'
+				instance.config.uploadImgHeaders = {
+					Authorization: state.token
+				}
+				instance.config.uploadImgParams = {
+					basket: 'img'
+				}
+				// 图片返回格式不同，需要自定义返回格式
+				instance.config.uploadImgHooks = {
+					// 图片上传并返回了结果，想要自己把图片插入到编辑器中
+					// 例如服务器端返回的不是 { errno: 0, data: [...] } 这种格式，可使用 customInsert
+					customInsert: function(insertImgFn, result) {
+						console.log('result', result)
+						// result 即服务端返回的接口
+						// insertImgFn 可把图片插入到编辑器，传入图片 src ，执行函数即可
+						insertImgFn(result.data.src)
+						// if (result.data && result.data.length) {
+						// 	result.data.forEach(item => insertImgFn(item))
+						// }
+					}
+				}
+				instance.config.uploadImgServer = uploadImgsServer
+				Object.assign(instance.config, {
+					onchange() {
+						console.log('change')
+					},
+				})
 				
+				instance.config.uploadVideoServer = uploadImgsServer
+				instance.config.uploadVideoMaxSize = 1 * 200 * 1024 * 1024 // 1024m
+				instance.config.uploadVideoAccept = ['mp4']
+				instance.config.uploadVideoParams = {
+				    basket: 'video',
+				}
+				instance.config.uploadVideoName = 'file'
+				instance.config.uploadVideoHeaders = {
+				    Authorization: state.token
+				}
+				
+				instance.config.uploadVideoHooks = {
+					// 视频上传并返回了结果，想要自己把视频插入到编辑器中
+					// 例如服务器端返回的不是 { errno: 0, data: { url : '.....'} } 这种格式，可使用 customInsert
+					customInsert: function(insertVideoFn, result) {
+						// result 即服务端返回的接口
+						console.log('customInsert', result)
+				
+						// insertVideoFn 可把视频插入到编辑器，传入视频 src ，执行函数即可
+						insertVideoFn(result.data.src)
+					}
+				}
+				
+				instance.create()
 				if (id) {
-					axios.get(`/api/back/technicalOfficeOutpatients/${id}`).then(res => {
-						state.Form = {
-							office_id: res.data.office_id,
-							yq_type: String(res.data.yq_type),
-							monday: res.data.monday,
-							tuesday: res.data.tuesday,
-							wednesday: res.data.wednesday,
-							thursday: res.data.thursday,
-							friday: res.data.friday,
-							saturday: res.data.saturday,
-							sunday: res.data.sunday,
-							type: String(res.data.type),
-							status: String(res.data.status),
+					axios.get(`/api/back/technicalOfficeOutpatientNews/${id}`).then(res => {
+						state.briefForm = {
+							content: res.data.content
+						}
+						if (instance) {
+							// 初始化商品详情 html
+							instance.txt.html(res.data.content)
 						}
 					})
 				}
 			})
-			
-			const get_technicaloffice_list = () => {
-				// const list = await axios.get('/api/back/technicalOffices')
-				// state.Form.list = list.data
-				axios.get('/api/back/technicalOffices').then(res => {
-					state.Form.list = res.data
-				})
-			}
+			onBeforeUnmount(() => {
+				instance.destroy()
+				instance = null
+			})
 			
 			const submitAdd = () => {
-				Ref.value.validate((vaild) => {
+				outpatientRef.value.validate((vaild) => {
 					if (vaild) {
 						// 默认新增用 post 方法
 						let httpOption = axios.post
 						let params = {
-							office_id: state.Form.office_id,
-							yq_type: state.Form.yq_type,
-							monday: state.Form.monday,
-							tuesday: state.Form.tuesday,
-							wednesday: state.Form.wednesday,
-							thursday: state.Form.thursday,
-							friday: state.Form.friday,
-							saturday: state.Form.saturday,
-							sunday: state.Form.sunday,
-							type: state.Form.type,
-							status: state.Form.status,
+							content: instance.txt.html(),
 						}
 			
-						let url = '/api/back/technicalOfficeOutpatients'
-			
+						let url = '/api/back/technicalOfficeOutpatientNews'
+						
 						if (id) {
 							// params.id = id
 							// 修改商品使用 put 方法
 							httpOption = axios.put
-							url = `/api/back/technicalOfficeOutpatients/${id}`
+							url = `/api/back/technicalOfficeOutpatientNews/${id}`
 						}
+			
 						httpOption(url, params).then(() => {
-							ElMessage.success(id ? '修改成功' : '添加成功')
+							if (id) {
+								ElMessage.success('修改成功')
+							} else {
+								ElMessage.success('添加成功')
+							}
 							router.push({
 								path: '/technicaloffice-outpatient'
 							})
-					})
+						})
 					}
 				})
 			}
-			
+
 			return {
 				...toRefs(state),
-				Ref,
-				submitAdd,
+				editor,
+				outpatientRef,
+				submitAdd
 			}
 		}
 	}
--- src/views/technicaloffice/outpatient/index.vue
+++ src/views/technicaloffice/outpatient/index.vue
@@ -1,129 +1,68 @@
 <template>
 	<el-card style="min-height: 100%;">
 		<template #header>
-			<el-button type="primary" :icon="Plus" @click="handleAdd">新增</el-button>
-			<div>
-				<el-select v-model="yq_type" placeholder="Select" filterable>
-					<el-option value="0" label="院本部"></el-option>
-					<el-option value="1" label="李庄院区"></el-option>
-				</el-select>
-				<el-input style="width: 200px; margin-top: 20px; margin-right: 10px; margin-left: 10px;" placeholder="请输入科室名称" v-model="technicaloffice_name" clearable />
-				<el-button type="primary" @click="handleOption">搜索</el-button>
+			<el-button v-if="flag" type="primary" :icon="Plus" @click="handleEdit(data.id)">编辑</el-button>
+			<el-button v-else type="primary" :icon="Plus" @click="handleAdd">新增</el-button>
+			<el-button v-if="data.status == 0 && flag" type="primary" :icon="Check" @click="handleCheck(data.id)">审核</el-button>
+			<div style="margin-top: 20px; margin-bottom: 20px;" v-if="flag">
+				<span>状态：</span>
+				<span style="color: #67C23A;" v-if="data.status == 1">已审核</span>
+				<span style="color: #E6A23C;" v-else>待审核</span>
 			</div>
 		</template>
-
-		<el-table v-loading="loading" :data="tableData" stripe style="width: 100%">
-			<el-table-column prop="id" label="id" />
-			<el-table-column prop="office_name" label="科室名称" />
-			<el-table-column prop="monday" label="周一" />
-			<el-table-column prop="tuesday" label="周二" />
-			<el-table-column prop="wednesday" label="周三" />
-			<el-table-column prop="thursday" label="周四" />
-			<el-table-column prop="friday" label="周五" />
-			<el-table-column prop="saturday" label="周六" />
-			<el-table-column prop="sunday" label="周天" />
-			<el-table-column prop="yq_type" label="院区">
-				<template #default="scope">
-					<span v-if="scope.row.yq_type == 0">院本部</span>
-					<span v-if="scope.row.yq_type == 1">李庄院区</span>
-				</template>
-			</el-table-column>
-			<el-table-column prop="type" label="上下午">
-				<template #default="scope">
-					<span v-if="scope.row.type == 0">上午</span>
-					<span v-if="scope.row.type == 1">下午</span>
-				</template>
-			</el-table-column>
-			<el-table-column prop="status" label="状态">
-				<template #default="scope">
-					<span style="color: #67C23A;" v-if="scope.row.status == 1">已审核</span>
-					<span style="color: #E6A23C;" v-else-if="scope.row.status == 0">待审核</span>
-					<span style="color: #F56C6C;" v-else>已删除</span>
-				</template>
-			</el-table-column>
-			<el-table-column prop="created_at" label="创建时间" />
-
-			<el-table-column label="操作" width="200">
-				<template #default="scope">
-					<a style="cursor: pointer; margin-right: 10px" v-if="scope.row.status != 2"
-						@click="handleEdit(scope.row.id)">修改</a>
-					<a style="cursor: pointer; margin-right: 10px" v-if="scope.row.status != 2"
-						@click="handleStatus(scope.row.id, 2)">删除</a>
-					<a style="cursor: pointer; margin-right: 10px" v-if="scope.row.status == 1"
-						@click="handleStatus(scope.row.id, 0)">撤销审核</a>
-					<a style="cursor: pointer; margin-right: 10px" v-else-if="scope.row.status == 0"
-						@click="handleStatus(scope.row.id, 1)">审核</a>
-				</template>
-			</el-table-column>
-		</el-table>
-
-		<el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
-			:current-page="currentPage" @current-change="changePage" />
+		<div v-html="data.content">
+		</div>
 	</el-card>
 </template>
 
 <script>
 	import {
-		Plus
+		Plus,
+		Check,
 	} from '@element-plus/icons-vue'
 	import {
-		onMounted,
 		reactive,
 		ref,
-		toRefs
+		toRefs,
+		onMounted
 	} from 'vue'
 	import axios from '@/utils/axios'
 	import {
 		ElMessage
 	} from 'element-plus'
 	import {
-		useRouter,
-		useRoute
+		useRoute,
+		useRouter
 	} from 'vue-router'
+	import {
+		sessionGet
+	} from '@/utils'
 
 	export default {
-		name: 'technicaloffice_setting_index',
 		setup() {
 			const router = useRouter()
 			const state = reactive({
-				yq_type: '0',
-				technicaloffice_name: '',
-				loading: false,
-				tableData: [], // 数据列表
-				total: 0, // 总条数
-				currentPage: 1, // 当前页
-				pageSize: 10 // 分页大小
+				flag: 0,
+				data: {
+					id: 0,
+					content: '',
+					status: 0,
+				},
 			})
+
 			onMounted(() => {
-				getOutpatientList()
+				getOutpatient()
 			})
 
-			const getOutpatientList = () => {
-				state.loading = true
-				axios.get('/api/back/technicalOfficeOutpatients', {
-					params: {
-						page: state.currentPage,
-						page_size: state.pageSize,
-						technicaloffice_name: state.technicaloffice_name,
-						yq_type: state.yq_type,
+			const getOutpatient = () => {
+				axios.get('/api/back/technicalOfficeOutpatientNews').then(res => {
+					if (res.data) {
+						state.data.content = res.data.content
+						state.data.id = res.data.id
+						state.data.status = res.data.status
+						state.flag = 1
 					}
-				}).then(res => {
-					state.tableData = res.data
-					state.pageSize = res.pagination.perPage
-					state.total = res.pagination.total
-					state.currentPage = res.pagination.currentPage
-					state.loading = false
 				})
-			}
-
-			const handleOption = () => {
-				state.currentPage = 1
-				getOutpatientList()
-			}
-
-			const changePage = (val) => {
-				state.currentPage = val
-				getOutpatientList()
 			}
 
 			const handleAdd = () => {
@@ -140,25 +79,23 @@
 					}
 				})
 			}
-
-			const handleStatus = (id, status) => {
-				axios.put('/api/back/technicalOfficeOutpatients/status', {
+			
+			const handleCheck = (id) => {
+				axios.put('/api/back/technicalOfficeOutpatientNews/status', {
 					id: id,
-					status: status
 				}).then(() => {
-					ElMessage.success('修改成功')
-					getOutpatientList()
+					ElMessage.success('审核成功')
+					window.location.reload()
 				})
 			}
 
 			return {
 				...toRefs(state),
-				handleOption,
-				changePage,
 				handleAdd,
 				handleEdit,
-				handleStatus,
+				handleCheck,
 				Plus,
+				Check,
 			}
 		}
 	}
