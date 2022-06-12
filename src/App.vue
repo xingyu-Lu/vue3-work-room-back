1d224ba52598c074fbd80cc6f011e31342fb3610
xingyu-Lu <2273466984@qq.com>
2022-06-12 11:14:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 					<div class="head">
 						<div>
 							<!-- <img src="/src/assets/hospital_logo.jpg" alt="logo"> -->
-							<a href="/" style="color: white; font-size: 18px;">宜宾市第三人民医院</a>
+							<a href="/" style="color: white; font-size: 18px;">吴淼胃肠疝工作室</a>
 						</div>
 					</div>
 					<div class="line" />
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="footer">
-    <div class="left">版权所有：@宜宾市第三人民医院</div>
+    <div class="left">版权所有：@吴淼胃肠疝工作室</div>
     <div class="right">
-		<el-button text>宜宾市第三人民医院官网后台管理系统</el-button>
+		<el-button text>吴淼胃肠疝工作室后台管理系统</el-button>
     </div>
   </div>
 </template>
